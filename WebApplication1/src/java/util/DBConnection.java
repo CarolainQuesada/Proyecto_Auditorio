f22a7aa854bb2e12cf285c3e17bbd440cc5ac300
Ashly Delgado <delgadomoralesashly1101@gmail.com>
2026-04-17 01:17:43 -0600

    Configure MySQL connection to Aiven cloud database

--- WebApplication1/src/java/util/DBConnection.java
+++ WebApplication1/src/java/util/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/auditorio";
-    private static final String USER = "root";
-    private static final String PASSWORD = "ashly321";
+    private static final String URL = "..";
+    private static final String USER = "..";
+    private static final String PASSWORD = "..";
 
     public static Connection getConnection() {
         try {
